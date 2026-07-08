810c57af32686baa58cd9edd6e0031aeb70d04ee
naghulvarshan <naghulvarshanbs@gmail.com>
2026-07-08 08:45:36 +0530

    cleaning up

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,19 @@
 package main
 
-import (
-	"log"
+// import (
+// 	"log"
 
-	"github.com/naghulvarshan/gosh/types"
-)
+// 	"github.com/naghulvarshan/gosh/types"
+// )
 
-func main() {
-	menu, _ := types.NewMenu(
-		[]string{"1. Apple", "2. Oranges", "3. Bananas"},
-		nil, nil)
-	inp := menu.GetUserInput()
-	if inp == -1 {
-		log.Println("No input selected")
-	} else {
-		log.Println("user selection is: ", menu.GetSelection(inp))
-	}
-}
+// func main() {
+// 	menu, _ := types.NewMenu(
+// 		[]string{"1. Apple", "2. Oranges", "3. Bananas"},
+// 		nil, nil)
+// 	inp := menu.GetUserInput()
+// 	if inp == -1 {
+// 		log.Println("No input selected")
+// 	} else {
+// 		log.Println("user selection is: ", menu.GetSelection(inp))
+// 	}
+// }
